eec73934058b0fed8aef914eb9b65f1cbbf81e12
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-02-09 22:01:07 +0100

    auth: implement functinality for user registration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Signup from './components/auth/Signup';
 import ProjectDetails from './components/projects/ProjectDetails';
 import ProjectList from './components/projects/ProjectList';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/projects" component={ProjectList} />
-        <Route path="/projects/:id" component={ProjectDetails} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { loggedInUser: null }
+
+  getTheUser= (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        {
+          this.state.loggedInUser ? 
+            <h1>Username: {this.state.loggedInUser.username}</h1> :
+            null
+        }
+
+        <Switch>
+          <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
+          <Route exact path="/projects" component={ProjectList} />
+          <Route path="/projects/:id" component={ProjectDetails} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
